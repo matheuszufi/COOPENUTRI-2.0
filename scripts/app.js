1cda294ccf19c81ef8bb5ed065e78d6a75fcfc5f
Matheus Zufi Leite <matheuszufi@gmail.com>
2023-02-09 15:49:09 -0300

    updatetop

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 
+let scrollPos = 0;
+const nav = document.getElementById('top');
+
+function checkPosition() {
+  let windowY = window.scrollY;
+  if (windowY < scrollPos) {
+    // Scrolling UP
+    nav.style.display = "flex";
+
+  } else {
+    // Scrolling DOWN
+    nav.style.display = "none";
+
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener('scroll', checkPosition);
